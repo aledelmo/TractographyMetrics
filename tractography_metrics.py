f6ca69d256f8dd5ac48cdf18076cb796e17fbb86
aledelmo <delmonte.ale92@gmail.com>
2018-08-31 14:13:37 +0200

    Corrected argument parsing

--- tractography_metrics.py
+++ tractography_metrics.py
@@ -5,15 +5,16 @@ import sys
 import argparse
 import os.path
 from time import time
-import processing as tm
+import processing_tm as tm
 
 __author__ = 'Alessandro Delmonte'
 __email__ = 'delmonte.ale92@gmail.com'
 
 
 def main():
-    tractogram, txt_filepath, fa_filepath, bzero_filepath, md_filepath, header, to_csv, to_xlsx = setup()
-    tm.proc(tractogram, txt_filepath, fa_filepath, bzero_filepath, md_filepath, header, to_csv, to_xlsx)
+    tractogram, txt_filepath, fa_filepath, bzero_filepath, md_filepath, header, to_csv, to_xlsx, perc_resampling = setup()
+    tm.proc(tractogram, txt_filepath, fa_filepath, bzero_filepath, md_filepath, header, to_csv, to_xlsx,
+            perc_resampling)
 
 
 def setup():
@@ -25,14 +26,16 @@ def setup():
     parser.add_argument('-bzero', '--b_zero', help='Name of the input b zero image file', type=check_nii)
     parser.add_argument('-md', '--Mean_Diffusivity', help='Name of the input mean diffusivity image file',
                         type=check_nii)
-    parser.add_argument('-h', '--header', help='Add header information tp the text file.', action=check_str)
+    parser.add_argument('-hd', '--header', help='Add header information to the text file.', type=check_str)
     parser.add_argument('-csv', '--save_csv', help='Save additional file in CSV format.', action='store_true')
     parser.add_argument('-xlsx', '--save_xlsx', help='Save additional file in Excel format.', action='store_true')
+    parser.add_argument('-r', '--resample', help='Downsampling streamlines (might improve computational time)',
+                        type=check_threshold)
 
     args = parser.parse_args()
 
     return (args.Input_Tractogram, args.Output_Stats, args.Fractional_Anisotropy, args.b_zero, args.Mean_Diffusivity,
-            args.header, args.save_csv, args.save_xlsx)
+            args.header, args.save_csv, args.save_xlsx, args.resample)
 
 
 def check_tracto(value):
@@ -45,7 +48,7 @@ def check_tracto(value):
 
 
 def check_txt(value):
-    if value.endswith('.txt') and os.path.isfile(os.path.abspath(value)):
+    if value.endswith('.txt'):
         return value
     else:
         raise argparse.ArgumentTypeError("Invalid output extension (file format supported: txt): %s" % value)
@@ -63,6 +66,19 @@ def check_str(value):
         return str(value)
     except:
         raise argparse.ArgumentTypeError("Invalid header information (must be a string): %s" % value)
+
+
+def check_threshold(value):
+    try:
+        t = float(value)
+        if 0 < t <= 100:
+            return t
+        else:
+            print('Suggestion: The resampling factor represent a percentage and must be between 0 and 100')
+            sys.exit(1)
+    except ValueError:
+        print('Suggestion: The resampling factor represent a percentage and must be between 0 and 100')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
